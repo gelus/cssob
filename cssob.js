427715363558528bc81b25611ed91e5dfa1fcaa8
Gelus <millervox@gmail.com>
2012-10-24 17:41:08 -0600

    support backed to IE7 && Syntax Adjustment
    Tested back to IE7 (emulated off IE9)
    Syntax Adjustment: Additional cssob methods now prototyped off each
    CSSStyleDeclaration (where available)

--- cssob.js
+++ cssob.js
@@ -1,22 +1,34 @@
 (function(){
 	window.cssob = {};
-	cssob.listProps = function(slector){
-		var Result = []; 
-		for(var i = 0; i<this[slector].length; i++){
-			Result.push(this[slector][i]);
-			}
-		return Result;
-		}
-	var stylesheets = document.styleSheets;
-	var crules = [];
-	for(var sti = 0; sti<stylesheets.length; sti++){
-		crules.push(stylesheets[sti]);
-	}
+	cssob.methods = [];
+	cssob.methods[0] = {name: 'listProps', func: function(){
+					var Result = []
+					for(var key in this){
+						if(this[key] != ""){
+							Result.push(key);
+						}}
+					return Result;
+				}};
+
+	cssob.composeMethods = function composeMethods(elm){
+		var meths = cssob.methods;
+		for(var i = 0; i<meths.length; i++){
+			elm[meths[i].name] = meths[i].func;
+		}}
+	function CSSStyleDeclarationCompensation(){this.n = false}
+	window.CSSStyleDeclaration = window.CSSStyleDeclaration || new CSSStyleDeclarationCompensation();
+	window.CSSRuleStyleDeclaration = window.CSSRuleStyleDeclaration || undefined;
+	var elm = (CSSRuleStyleDeclaration || CSSStyleDeclaration).prototype;
+	if(CSSStyleDeclaration.n === false){elm = CSSStyleDeclaration;}
+	cssob.composeMethods(elm);//Method Assignment
+	
+	crules = document.styleSheets;
 	for(var qi = 0; qi<crules.length; qi++){
 		ruleSet = crules[qi].cssRules || crules[qi].rules;
 		for(var rsi = 0;rsi<ruleSet.length; rsi++){
 			var rule = ruleSet[rsi];
-			cssob[rule.selectorText] = rule.style;
+			if(CSSStyleDeclaration.n === false){cssob.composeMethods(rule.style);} //IE7 method Assignment
+			cssob[rule.selectorText.toLowerCase()] = rule.style;
 		}
 	}
 })();
